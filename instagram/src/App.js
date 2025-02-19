c60e6e90b7924bcf0c5c108c10111da788053661
thebigcoderheehee <singhharfateh47@gmail.com>
2025-02-18 20:40:48 -0500

    remove unused files and clean up project structure

---
+++ instagram/src/App.js
@@ -0,0 +1,13 @@
+import Left_section from './components/Left_section';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Left_section/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ instagram/src/components/Left_section.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Left_section() {
+  return (
+    <div>left_section</div>
+  )
+}
+
+export default Left_section
